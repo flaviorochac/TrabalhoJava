f023e34b46198ac9f80d562652290b0419f95277
Flávio Rocha Cezarino <ghostreapedrc@gmail.com>
2016-06-14 20:34:15 -0300

    Configurado o projeto, criado as classes com get end set para seus atributos.

---
+++ Trabalho15.5109/src/main/java/br/univel/Cliente.java
@@ -0,0 +1,85 @@
+package br.univel;
+
+public class Cliente {
+
+	private int id;
+	private String nome;
+	private String endereco;
+	private int numero;
+	private String complemneto;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String cep;
+	private String telefone;
+	private String celular;
+
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getComplemneto() {
+		return complemneto;
+	}
+	public void setComplemneto(String complemneto) {
+		this.complemneto = complemneto;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+}
